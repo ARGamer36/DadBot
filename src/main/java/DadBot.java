a1a3da4a153904b7a4d06aec876a2d197e7c7e9a
ARGamer36 <ajrodz2004@gmail.com>
2023-10-21 17:11:08 -0400

    Initial Commit

--- src/main/java/DadBot.java
+++ src/main/java/DadBot.java
@@ -1,10 +1,24 @@
 import Main.BotDriver;
 import net.dv8tion.jda.api.entities.Activity;
 
+import javax.security.auth.login.LoginException;
+
 public class DadBot extends BotDriver {
     @Override
     protected void setup() {
         VERSION = "1.0";
-        activity = Activity.watching("Test Bot");
+        activity = Activity.watching("You grow up so fast");
+        TOKEN = "DAD_BOT";
+    }
+    public DadBot() throws LoginException {
+        super();
+        shardManager.addEventListener(new DadMain(VERSION));
+    }
+    public static void main(String[] args) {
+        try {
+            DadBot bot = new DadBot();
+        } catch (LoginException e) {
+            System.out.println("ERROR: provided bot token is invalid");
+        }
     }
 }
---
+++ src/main/java/DadMain.java
@@ -0,0 +1,7 @@
+import Main.MainCommands;
+
+public class DadMain extends MainCommands {
+    public DadMain(String version) {
+        super(version);
+    }
+}
