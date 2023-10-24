c321e2555d2d77e2b762a78106746f77a3a652e7
ARGamer36 <ajrodz2004@gmail.com>
2023-10-24 02:29:20 -0400

    GiveJoke renamed command

--- src/main/java/commands/dadJokes/GiveJoke.java
+++ src/main/java/commands/dadJokes/GiveJoke.java
@@ -5,7 +5,7 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class GiveJoke extends PrefixCommand {
     public GiveJoke() {
-        name = "giveJoke";
+        name = "dadJoke";
         description = "Give pop a chance to tell you a joke";
     }
 
